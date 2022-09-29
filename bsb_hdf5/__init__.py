8b1bedb08270f7ee29faccdaabb8f0726c69a295
Robin De Schepper <robin.deschepper93@gmail.com>
2022-09-29 17:27:35 +0200

    bump to 0.4.1

--- bsb_hdf5/__init__.py
+++ bsb_hdf5/__init__.py
@@ -12,7 +12,7 @@ import os
 import shutil
 import shortuuid
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "PlacementSet",
     "ConnectivitySet",
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 requires = [
-    "bsb~=4.0.0a40",
+    "bsb~=4.0.0a41",
     "shortuuid",
 ]
 
