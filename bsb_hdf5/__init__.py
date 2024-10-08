27f76869e86dda43653201bbc5af60c544dbd1f3
filimarc <filimarc@users.noreply.github.com>
2024-10-08 08:55:45 +0000

    docs: bump version: v4.0.0 → v4.1.0 [skip ci]

--- bsb_hdf5/__init__.py
+++ bsb_hdf5/__init__.py
@@ -19,7 +19,7 @@ from .file_store import FileStore
 from .morphology_repository import MorphologyRepository
 from .placement_set import PlacementSet
 
-__version__ = "4.0.0"
+__version__ = "4.1.0"
 __all__ = [
     "PlacementSet",
     "ConnectivitySet",
